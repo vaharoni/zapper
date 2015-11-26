a78f835733b7089bff35de118fadd9dd9882402f
Amit Aharoni <amit.sites@gmail.com>
2015-11-25 21:52:41 -0800

    Set port better

--- node-server.js
+++ node-server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
@@ -142,5 +143,6 @@ function clientObjToStr(obj) {
 	return {stamp: obj["stamp"], clientId: obj["clientId"], active: obj["active"]};
 }
 
-// s.listen(8000);
-app.listen(80);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
